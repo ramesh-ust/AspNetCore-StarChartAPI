b9f3f3fad110a1e3eecd2fc12a921d32ec4869ba
ramesh-ust <ramesh.rajappan@ust-global.com>
2021-08-13 05:00:42 +0530

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +17,57 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var currCelObj = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (currCelObj == null)
+            {
+                return NotFound();
+            }
+            var currSatelites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
+            currCelObj.Satellites = currSatelites;
+            return Ok(currCelObj);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var currCelObj = _context.CelestialObjects.FirstOrDefault(x => x.Name == name);
+            if (currCelObj == null)
+            {
+                return NotFound();
+            }
+            var currSatelites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == currCelObj.Id).ToList();
+            currCelObj.Satellites = currSatelites;
+            return Ok(currCelObj);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var cList = _context.CelestialObjects.ToList();
+            for (int i = 0; i < cList.Count; i++)
+            {
+                var outer = cList[i];
+                for (int j = 0; j < cList.Count; j++)
+                {
+                    var inner = cList[j];
+                    if(outer.Id== inner.OrbitedObjectId)
+                    {
+                        if (outer.Satellites == null)
+                        {
+                            outer.Satellites = new List<CelestialObject>();
+                        }
+                        outer.Satellites.Add(inner);
+                    }
+
+                }
+            }
+            return Ok(cList);
         }
     }
 }
