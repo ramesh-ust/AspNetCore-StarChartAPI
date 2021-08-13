04a5c908a1157db3b102218c3ffa43a6ae458113
ramesh-ust <ramesh.rajappan@ust-global.com>
2021-08-13 05:36:33 +0530

    Create CelestialObjectController's Get Actions Fix

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -36,14 +36,30 @@ namespace StarChart.Controllers
         [HttpGet("{name}")]
         public IActionResult GetByName(string name)
         {
-            var currCelObj = _context.CelestialObjects.FirstOrDefault(x => x.Name == name);
-            if (currCelObj == null)
+            var cList = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+            if (cList.Count==0)
             {
                 return NotFound();
             }
-            var currSatelites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == currCelObj.Id).ToList();
-            currCelObj.Satellites = currSatelites;
-            return Ok(currCelObj);
+            var allList = _context.CelestialObjects.ToList();
+            for (int i = 0; i < cList.Count; i++)
+            {
+                var outer = cList[i];
+                for (int j = 0; j < allList.Count; j++)
+                {
+                    var inner = allList[j];
+                    if (outer.Id == inner.OrbitedObjectId)
+                    {
+                        if (outer.Satellites == null)
+                        {
+                            outer.Satellites = new List<CelestialObject>();
+                        }
+                        outer.Satellites.Add(inner);
+                    }
+
+                }
+            }
+            return Ok(cList);
         }
 
         [HttpGet]
